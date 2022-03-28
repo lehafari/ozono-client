b8dec3c1c25f1b10018724963461961323f294ea
Jorge Quintero <rouchjq8@gmail.com>
2022-03-28 18:56:45 -0400

    Creando contexto de usuario
    Se esta creando el Reduce (estado inicial) y el contexto del usuario para la aplicacion

--- src/components/common/Header/styles.jsx
+++ src/components/common/Header/styles.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   background: ${(props) => props.theme.colors.headerBlue};
@@ -7,12 +7,8 @@ export const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 2rem;
+  padding: 0px 2rem;
   box-sizing: border-box;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 10;
 `;
 
 export const Container1 = styled.div`
