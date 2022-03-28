4f6d51cdb4b5bc023c5a8049dcdf521779061ba9
Jorge Quintero <rouchjq8@gmail.com>
2022-03-28 16:58:19 -0400

    routes
    Se establecio todas las rutas de las paginas necesarias

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Header } from './components/common/Header';
+import { Header } from "./components/common/Header";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AppRouter />
     </div>
   );
 }
---
+++ src/pages/Contac/Contac.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contac = () => {
+  return (
+    <div>
+      <h1>Contac</h1>
+      <hr />
+    </div>
+  );
+};
+
+export default Contac;
