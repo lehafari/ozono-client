b6edd99c9046427e5420cbaa62deeac213695d99
Jorge Quintero <rouchjq8@gmail.com>
2022-04-04 16:10:15 -0400

    retoqeus finales navbar course

--- src/components/layout/CoursePage/CourseNavbar/index.jsx
+++ src/components/layout/CoursePage/CourseNavbar/index.jsx
@@ -15,24 +15,24 @@ const CourseNavbar = () => {
       <ImgContainer>
         <Button
           text="Entrar a clase"
-          padding="1.1rem 4rem"
+          padding="1.1rem 5rem"
           margin=" 7rem 0rem "
         />
         <NavbarContenedor>
           <MenuContainer>
-            <NavButton color="#797979" fontSize="1.2rem" fontWeight="400">
+            <NavButton color="#797979" fontSize="1.2rem" fontWeight="300">
               <Link to="/course/description">Descripcion</Link>
             </NavButton>
-            <NavButton color="#797979" fontSize="1.2rem" fontWeight="400">
+            <NavButton color="#797979" fontSize="1.2rem" fontWeight="300">
               <Link to="/course/teachers">Profesores</Link>
             </NavButton>
-            <NavButton color="#797979" fontSize="1.2rem" fontWeight="400">
+            <NavButton color="#797979" fontSize="1.2rem" fontWeight="300">
               <Link to="/course/Curriculum">Curriculum</Link>
             </NavButton>
-            <NavButton color="#797979" fontSize="1.2rem" fontWeight="400">
+            <NavButton color="#797979" fontSize="1.2rem" fontWeight="300">
               <Link to="/course/certificates">Certificados</Link>
             </NavButton>
-            <NavButton color="#797979" fontSize="1.2rem" fontWeight="400">
+            <NavButton color="#797979" fontSize="1.2rem" fontWeight="300">
               <Link to="/course/comments">Comentarios</Link>
             </NavButton>
           </MenuContainer>
