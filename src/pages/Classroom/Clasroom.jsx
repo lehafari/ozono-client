83a7d2c0fd0a8fa981f9d60169020221d3ed16ed
Jorge Quintero <rouchjq8@gmail.com>
2022-04-07 11:11:29 -0400

    Clasrrom en proceso

--- src/pages/Classroom/Clasroom.jsx
+++ src/pages/Classroom/Clasroom.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React from "react";
+import { BackgroundNavbar } from "../../components/common/BackgroundNavbar";
+import { MenuBar } from "../../components/common/MenuBar";
+import {
+  HeaderList,
+  Image,
+  ImgContainer,
+  ImgItem,
+  ItemList,
+  List,
+  SectionContainer,
+  Text,
+  TextItem,
+  Title,
+  VideoList,
+  VideoTitle,
+} from "./styles";
 
 const Classroom = () => {
   return (
-    <div>
-      <h1>Classroom</h1>
-      <hr />
-    </div>
+    <>
+      <BackgroundNavbar />
+      <MenuBar />
+      <SectionContainer>
+        <ImgContainer>
+          <Image>
+            <img src="" alt="" />
+          </Image>
+          <VideoTitle>
+            <h2>OLA SOY UN TITULO</h2>
+          </VideoTitle>
+        </ImgContainer>
+        <VideoList>
+          <HeaderList>
+            <Title>Titulo del curso</Title>
+            <Text>
+              <div>
+                <h5>Video</h5>
+              </div>
+              <div>
+                <h5>Recursos</h5>
+              </div>
+            </Text>
+          </HeaderList>
+          <List>
+            <ItemList>
+              <ImgItem>Miniatura</ImgItem>
+              <TextItem>Titulo de la miniatura</TextItem>
+            </ItemList>
+          </List>
+        </VideoList>
+      </SectionContainer>
+    </>
   );
 };
 
---
+++ src/pages/Classroom/styles.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const SectionContainer = styled.div`
+  padding: 0px 100px 0px 100px;
+`;
+
+export const ImgContainer = styled.div``;
+
+export const Image = styled.div``;
+
+export const VideoTitle = styled.div``;
+
+export const VideoList = styled.div``;
+
+export const HeaderList = styled.div``;
+
+export const Title = styled.div``;
+
+export const Text = styled.div``;
+
+export const List = styled.div``;
+
+export const ItemList = styled.div``;
+
+export const ImgItem = styled.div``;
+
+export const TextItem = styled.div``;
