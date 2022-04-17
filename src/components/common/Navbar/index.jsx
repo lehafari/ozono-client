d9eddf1ff9b21f636269c6d0597523adb7f1ae9e
Alejandro <alejomedina161011@gmail.com>
2022-04-17 19:44:17 -0400

    avatar

--- src/components/common/Navbar/index.jsx
+++ src/components/common/Navbar/index.jsx
@@ -11,13 +11,15 @@ import {
   SignupButton,
 } from './styles';
 import cenaozLogo from '../../../assets/images/cenaoz-logo.webp';
-
+import profileImage from '../../../assets/images/profile-image.svg';
 // import { useContext, useEffect, useState } from "react";
 // import { AuthContext } from "../../../auth/authContext";
 // import { types } from "../../../types/types";
 import { Link, useNavigate } from 'react-router-dom';
 import MobileMenu from '../MobileMenu';
 import Resize from '../../../helpers/Resize';
+import { useSelector } from 'react-redux';
+import { Avatar } from '@mui/material';
 export const Navbar = () => {
   const width = Resize();
   const navigate = useNavigate();
@@ -51,7 +53,19 @@ export const Navbar = () => {
               </SignupButton>
             </LoginContainer>
           ) : (
-            <AvatarContainer></AvatarContainer>
+            <AvatarContainer>
+              <Avatar
+                alt="Remy Sharp"
+                src={profileImage}
+                sx={{
+                  width: '50px',
+                  height: '50px',
+                  cursor: 'pointer',
+                }}
+                onClick={() => navigate('/profile')}
+              />
+              <h1>{user.username}</h1>
+            </AvatarContainer>
           )}
         </NavbarMenu>
       ) : (
