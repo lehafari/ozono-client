95e3f39835f04a001553c449f3db4c22fd1c446a
Jorge Quintero <rouchjq8@gmail.com>
2022-04-17 20:31:40 -0400

    proteccion de rutas publicas

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { Route, Redirect } from 'react-router-dom';
-
+import { Route, Redirect } from "react-router-dom";
 
 export const PrivateRoute = ({
-    isAuthenticated,
-    component: Component,
-    ...rest
+  isAuthenticated,
+  component: Component,
+  ...rest
 }) => {
-
-    return (
-        <Route { ...rest }
-            component={ (props) => (
-                ( isAuthenticated )
-                    ? ( <Component { ...props } /> )
-                    : ( <Redirect to="/login" /> )
-            )}
-        
-        />
-    )
-}
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/access/login" />
+        )
+      }
+    />
+  );
+};
 
 PrivateRoute.propTypes = {
-    isAuthenticated: PropTypes.bool.isRequired,
-    component: PropTypes.func.isRequired
-}
+  isAuthenticated: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired,
+};
