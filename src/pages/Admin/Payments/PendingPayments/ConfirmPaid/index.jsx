23013058c8ef1b087b790fa3a2caf0c6735e3585
Jorge Quintero <rouchjq8@gmail.com>
2022-06-23 15:41:52 -0400

    vista del administrador para aceptar pagos

--- src/pages/Admin/Payments/PendingPayments/ConfirmPaid/index.jsx
+++ src/pages/Admin/Payments/PendingPayments/ConfirmPaid/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import FactCheckIcon from "@mui/icons-material/FactCheck";
 import React from "react";
 import { Dialog } from "primereact/dialog";
@@ -18,9 +18,78 @@ const ConfirmPaid = (props) => {
         onHide={() => setVisible(false)}
         draggable={false}
         resizable={false}
+        style={{
+          width: "70vw",
+          height: "70vh",
+        }}
       >
         <Box>
-          <TextField />
+          <Typography
+            sx={{
+              fontSize: "1.5rem",
+              margin: "1rem 0",
+            }}
+          >
+            Producto
+          </Typography>
+          <TextField label="Curso que pago" sx={{ margin: "0 10px" }} />
+        </Box>
+        <Typography
+          sx={{
+            fontSize: "1.5rem",
+            margin: "1rem 0",
+          }}
+        >
+          Datos de la persona
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            margin: "0.5rem 0rem",
+            flexShrink: "0",
+          }}
+        >
+          <TextField label="name" sx={{ margin: "0 10px" }} />
+          <TextField label="email" sx={{ margin: "0 10px" }} />
+          <TextField label="tlf" sx={{ margin: "0 10px" }} />
+          <TextField label="date" sx={{ margin: "0 10px" }} />
+        </Box>
+        <Typography
+          sx={{
+            fontSize: "1.5rem",
+            margin: "1rem 0",
+          }}
+        >
+          Datos del pago
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            margin: "0.5rem 0rem",
+            flexShrink: "0",
+          }}
+        >
+          <TextField label="Tipo de pago" sx={{ margin: "0 10px" }} />
+          <TextField label="Referencia" sx={{ margin: "0 10px" }} />
+          <TextField label="Monto" sx={{ margin: "0 10px" }} />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            margin: "1rem 0rem",
+          }}
+        >
+          <Button variant="contained" color="success" sx={{ margin: "0 1rem" }}>
+            Aceptar
+          </Button>
+          <Button variant="contained" color="error">
+            Rechazar
+          </Button>
         </Box>
       </Dialog>
     </>
